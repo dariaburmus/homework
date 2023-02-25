b3953a2a21f5a0e7630bb905dd39617a6519af02
Daria Burmus <burmusdarya2@gmail.com>
2023-02-26 01:13:53 +0200

    Homework 3. Calculator v1.2

---
+++ homework_3/calculation_2.js
@@ -0,0 +1,43 @@
+const operation = prompt(
+	'Choose operation: ', 
+	'sum, substraction, multiplication, division, pow, sin, cos'
+);
+
+let result = 0;
+
+if (
+	operation === 'sum' ||
+	operation === 'substraction' ||
+	operation === 'multiplication' ||
+	operation === 'division' ||
+	operation === 'pow'
+) {
+	const firstOperand = +prompt('First operand: ', 0);
+	const secondOperand = +prompt('Second operand: ', 0);
+
+	if (operation === 'sum') {
+		result = firstOperand + secondOperand;
+	} else if (operation === 'substraction') {
+		result = firstOperand - secondOperand;
+	} else if (operation === 'multiplication') {
+		result = firstOperand * secondOperand;
+	} else if (operation === 'division') {
+		result = firstOperand / secondOperand;
+	} else {
+		result = firstOperand ** secondOperand;
+	}	
+
+	alert(`Calculation finished: ${result}`);
+} else if (operation === 'sin' || operation === 'cos') {
+	const radians = +prompt('Radians: ', 0);
+
+	if (operation === 'sin') {
+		result = Math.sin(radians);
+	} else {
+		result = Math.cos(radians);
+	}
+
+	alert(`Calculation finished: ${result}`);
+} else {
+	alert('No such operation');
+}
